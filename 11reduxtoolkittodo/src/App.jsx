77707e873850747b4c5e3bb2148eac94f8d5b1e4
devanshagrawal-169 <devanshagrawal169@gmail.com>
2024-03-07 02:42:58 +0530

    redux_toolkit_todo

---
+++ 11reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1 className = "text-3xl text-center" >To Do App</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
