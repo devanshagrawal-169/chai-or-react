53bb88fa975566af99a92def14615f520fd38e9b
devanshagrawal-169 <devanshagrawal169@gmail.com>
2024-03-11 03:26:53 +0530

    done project

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,19 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup"
+import Login from "./Login"
+import PostForm from "./post-form/PostForm";
+import Select from "./Select";
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Input from "./Input"
+import Button from "./Button";
+
+
+export {
+    Header,Footer, Container,Logo, LogoutBtn, RTE, Signup, Login, PostForm,Select, PostCard,AuthLayout, Input,Button
+}
